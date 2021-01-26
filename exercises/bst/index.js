29222c3910fe4e695832e46b737ff9181f89f785
Sum Xu <v-suxu@expedia.com>
2021-01-26 16:23:06 +0800

    bst

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,62 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data
+    this.left = null
+    this.right = null
+  }
+
+  insert(data) {
+    if (data < this.data) {
+      if (!this.left) {
+        this.left = new Node(data)
+      } else {
+        this.left.insert(data)
+      }
+    } else {
+      if (!this.right) {
+        this.right = new Node(data)
+      } else {
+        this.right.insert(data)
+      }
+    }
+  }
+
+  contains(data) {
+    if (data === this.data) return this
+
+    if (data < this.data) {
+      if (!this.left) {
+        return null
+      } else {
+        if (this.left.data === data) {
+          return this.left
+        } else {
+          return this.left.contains(data) // need to return here, very important!!!
+        }
+      }
+    } else {
+      if (!this.right) {
+        return null
+      } else {
+        if (this.right.data === data) {
+          return this.right
+        } else {
+          return this.right.contains(data) // need to return here, very important!!!w
+        }
+      }
+    }
+  }
+}
+
+const node = new Node(4)
+node.insert(9)
+node.insert(2)
+node.insert(5)
+
+const n = node.contains(5)
+console.log('n', n);
 
 module.exports = Node;
